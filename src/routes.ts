e261d658c960f38e4c3b28d3f7e1d1fbf05984c6
Antonio Arauujo Pavao <antonioppavao@gmail.com>
2021-10-27 13:52:52 -0300

    Adding CreateMessage route

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateMessageController } from "./controllers/CreateMessageController";
 
 const router = Router();
 
 router.post("/authenticate", new AuthenticateUserController().handle);
+
+router.post("/messages", new CreateMessageController().handle);
 
 export { router };

4b838568461e753e0228109580a2d99ede775b96
Antonio Arauujo Pavao <antonioppavao@gmail.com>
2021-10-27 13:47:14 -0300

    Creating ensureAuthenticate Middleware

--- src/services/CreateMessageService.ts
+++ src/services/CreateMessageService.ts
@@ -1,4 +1,17 @@
+import prismaClient from "../prisma";
+
 class createMessageService {
-  async execute(text: string, user_id: string) {}
+  async execute(text: string, user_id: string) {
+    const message = await prismaClient.message.create({
+      data: {
+        text,
+        user_id,
+      },
+      include: {
+        user: true,
+      },
+    });
+    return message;
+  }
 }
 export { createMessageService };

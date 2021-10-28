d13ecbc4a566f554a044f63da9bfb79751e09521
Antonio Arauujo Pavao <antonioppavao@gmail.com>
2021-10-27 21:43:28 -0300

    Creating public folder

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,26 @@
 import "dotenv/config";
 import express from "express";
 import { authRouter } from "./routes/authRoute";
-
 import { messageRouter } from "./routes/messagesRoute";
 import { userRouter } from "./routes/userRoute";
+import http from "http";
+import cors from "cors";
+import { Server } from "socket.io";
 
 const app = express();
+app.use(cors());
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, { cors: { origin: "*" } });
+
+io.on("connection", (socket) => {
+  console.log(`Usuario conectado no socket ${socket.id}`);
+});
 
 app.use(express.json());
+
 app.use(authRouter);
 app.use(userRouter);
 app.use(messageRouter);
 
-app.listen(4000, () => console.log(`Server is running on port 4000`));
+serverHttp.listen(4000, () => console.log(`Server is running on port 4000`));
--- src/services/CreateMessageService.ts
+++ src/services/CreateMessageService.ts
@@ -11,6 +11,7 @@ class CreateMessageService {
         user: true,
       },
     });
+
     return message;
   }
 }
